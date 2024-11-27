502cd4f4d24b750dae521d27c9f9cd39d87dbc6d
danfasst <danielcfcunha2005@gmail.com>
2024-11-26 22:36:52 -0300

    decimo commit

--- app/src/main/java/br/edu/up/rgm32662921/InventoryApplication.kt
+++ app/src/main/java/br/edu/up/rgm32662921/InventoryApplication.kt
@@ -25,7 +25,7 @@ class InventoryApplication : Application() {
     /**
      * AppContainer instance used by the rest of classes to obtain dependencies
      */
-    lateinit var container: AppContainer
+    private lateinit var container: AppContainer
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/br/edu/up/rgm32662921/data/ItemDao.kt
+++ app/src/main/java/br/edu/up/rgm32662921/data/ItemDao.kt
@@ -20,10 +20,9 @@ interface ItemDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(item: Item)
 
-    @Query("SELECT * from items WHERE id = :id")
-    fun getItem(id: Int): Flow<Item>
-
     @Query("SELECT * from items ORDER BY name ASC")
     fun getAllItems(): Flow<List<Item>>
 
+    @Query("SELECT * from items WHERE id = :id")
+    fun getItem(id: Int): Flow<Item>
 }
